a1b9ce75758816d885a2357db38c8fc0bd999cdb
Chang Liu <cliu@adobe.com>
2020-05-21 22:40:00 -0700

    update to example node programs

--- 4-modules/6-async-patterns/3-cb-nesting.js
+++ 4-modules/6-async-patterns/3-cb-nesting.js
@@ -1,9 +1,40 @@
 const fs = require('fs');
 
 fs.readFile(__filename, function cb1(err, data) {
+    console.log('read done')
+    for (i = 0; i < 1000000; i++) {
+      if ( i % 100000 == 0){
+          console.log(i)
+      };
+    }
   fs.writeFile(__filename + '.copy', data, function cb2(err) {
     // Nest more callbacks here...
+    for (i = 0; i < 100000; i++) {
+      if ( i % 10000 == 0){
+          console.log(i)
+      };
+    }
+    console.log('first write done')
   });
 });
 
+fs.readFile(__filename, function cb1(err, data) {
+    console.log('second read done')
+    for (i = 0; i < 1000000; i++) {
+      if ( i % 200000 == 0){
+          console.log(i)
+      };
+    }
+  fs.writeFile(__filename + '.copy', data, function cb2(err) {
+    console.log('second write done')
+  });
+});
+
+
+
+for (i = 0; i < 1000000; i++) {
+  if ( i % 100000 == 0){
+      console.log(i)
+  };
+}
 console.log('TEST');
--- 4-modules/7-event-emitters/example.js
+++ 4-modules/7-event-emitters/example.js
@@ -1,4 +1,14 @@
 const EventEmitter = require('events');
 
+const myEmitter = new EventEmitter()
+
+myEmitter.on('TEST_EVENT',() => {
+    console.log('TEST_EVENT was fired')
+})
+
+myEmitter.emit('TEST_EVENT')
+
+
+
 // Streams are Event Emitters
 // process.stdin, process.stdout
